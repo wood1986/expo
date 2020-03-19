f510e5bb980af9679ecea4f11017558b23f4c580
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-03-19 12:19:11 +0100

    [file system] Add multipart upload

--- packages/expo-file-system/android/src/main/java/expo/modules/filesystem/FileSystemModule.java
+++ packages/expo-file-system/android/src/main/java/expo/modules/filesystem/FileSystemModule.java
@@ -37,6 +37,7 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.math.BigInteger;
 import java.net.CookieHandler;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.HashMap;
@@ -44,14 +45,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.JavaNetCookieJar;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -511,13 +511,50 @@ public class FileSystemModule extends ExportedModule {
         }
       }
 
-      RequestBody body = RequestBody.create(null, uriToFile(fileUri));
-
+      File file = uriToFile(fileUri);
       HTTPMethod method = HTTPMethod.POST;
       if (options.containsKey("httpMethod")) {
         method = importHttpMethod((int) (double) options.get("httpMethod"));
       }
-      requestBuilder.method(method.toString(), body);
+
+      int type = 0;
+      if (options.containsKey("uploadType")) {
+        type = (int) (double) options.get("uploadType");
+      }
+
+      if (isRawUpload(type)) {
+        RequestBody body = RequestBody.create(null, file);
+        requestBuilder.method(method.toString(), body);
+      } else {
+        MultipartBody.Builder bodyBuilder = new MultipartBody.Builder()
+          .setType(MultipartBody.FORM);
+
+        if (options.containsKey("parameters")) {
+          Map<String, Object> parametersMap = (Map<String, Object>) options.get("parameters");
+          for (String key : parametersMap.keySet()) {
+            String value = String.valueOf(parametersMap.get(key));
+
+            bodyBuilder.addFormDataPart(key, value);
+          }
+        }
+
+        String mimeType;
+        if (options.containsKey("mimeType")) {
+          mimeType = (String) options.get("mimeType");
+        } else {
+          mimeType = URLConnection.guessContentTypeFromName(file.getName());
+        }
+
+        String fieldName = file.getName();
+        if (options.containsKey("fieldName")) {
+          fieldName = (String) options.get("fieldName");
+        }
+
+        bodyBuilder.addFormDataPart(fieldName, file.getName(), RequestBody.create(mimeType != null ? MediaType.parse(mimeType) : null, file));
+
+        requestBuilder.method(method.toString(), bodyBuilder.build());
+      }
+
 
       getOkHttpClient().newCall(requestBuilder.build()).enqueue(new Callback() {
         @Override
@@ -1013,5 +1050,9 @@ public class FileSystemModule extends ExportedModule {
 
   private HTTPMethod importHttpMethod(int method) {
     return HTTPMethod.fromInt(method);
+  }
+
+  private boolean isRawUpload(int type) {
+    return type == 0;
   }
 }
